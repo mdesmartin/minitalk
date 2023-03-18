21a7bc519004b1ce49950cec5d6b0e435a044688
Mehdi Vogel <mvogel@z4r2p3.42lyon.fr>
2023-03-18 16:38:42 +0100

    FIX: client/added ping pong mechanysm instead of usleep server/handle pid with sigaction flag

--- src/client.c
+++ src/client.c
@@ -6,44 +6,52 @@
 /*   By: mvogel <mvogel@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 14:29:25 by mvogel            #+#    #+#             */
-/*   Updated: 2023/03/18 12:58:47 by mvogel           ###   ########lyon.fr   */
+/*   Updated: 2023/03/18 16:15:41 by mvogel           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 #include <signal.h>
 
+int	bit_received;
+
+void	confirm_reception(int signum)
+{
+	if (signum == SIGUSR1)
+		bit_received = 1;
+	if (signum == SIGUSR2)
+	{
+		ft_putstr_fd("Message received\n", 1);
+		exit(0);
+	}
+}
+
 void	send_bit(unsigned int val, pid_t pid_server, int bit)
 {
 	while (bit != 0)
 	{
+		bit_received = 0;
 		if (val % 2 == 0)
 			kill(pid_server, SIGUSR1);
 		if (val % 2 == 1)
 			kill (pid_server, SIGUSR2);
 		val /= 2;
 		bit--;
-		usleep(100);
+		while (!bit_received)
+			pause();
 	}
-}
-
-void	confirm_reception(int sigusr)
-{
-	(void) sigusr;
-	ft_putstr_fd("Message received\n", 1);
-	exit(0);
 }
 
 int	main(int argc, char *argv[])
 {	
 	struct sigaction	sign;
-	unsigned int		pid_server;
+	pid_t				pid_server;
 	int					i;
 
-	i = 0;
-	sign.sa_handler = confirm_reception;
 	sigemptyset(&sign.sa_mask);
 	sign.sa_flags = SA_SIGINFO;
+	sign.sa_handler = confirm_reception;
+	i = 0;
 
 	if (argc != 3)
 		return (ft_putstr_fd("Error\nClient need server PID and a string\n", \
@@ -52,7 +60,7 @@ int	main(int argc, char *argv[])
 	if (kill(pid_server, 0) != 0)
 		return (ft_putstr_fd("Error\nServer PID is not correct\n", 2), -1);
 	sigaction(SIGUSR1, &sign, NULL);
-	send_bit(getpid(), pid_server, 32);
+	sigaction(SIGUSR2, &sign, NULL);
 	while (argv[2] != NULL && argv[2][i] != 0)
 		send_bit(argv[2][i++], pid_server, 8);
 	send_bit(0, pid_server, 8);
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: mvogel <mvogel@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 15:00:35 by mvogel            #+#    #+#             */
-/*   Updated: 2023/03/18 12:55:41 by mvogel           ###   ########lyon.fr   */
+/*   Updated: 2023/03/18 16:22:10 by mvogel           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,56 +18,36 @@ typedef struct s_bit
 {
 	unsigned char	value;
 	int				i;
-	bool			status;
-	int				pid_client;
 }	t_bit;
 
-void	client_pid(t_bit *bit)
+void	get_bit(int signum, siginfo_t *info, void *contex)
 {
-	bit->status = true;
-	bit->value = 0;
-	bit->i = 0;
-}
-
-void	write_msg(t_bit *bit)
-{
-	write(1, &(bit->value), 1);
-	if (bit->value == 0)
-	{
-		kill(bit->pid_client, SIGUSR1);
-		bit->status = false;
-		bit->pid_client = 0;
-	}
-	bit->value = 0;
-	bit->i = 0;
-}
-
-void	get_bit(int sigusr)
-{
+	pid_t 			pid_client;
 	static t_bit	bit;
-	bool			bin;
 
-	if (sigusr == SIGUSR1)
-		bin = 0;
-	if (sigusr == SIGUSR2)
-		bin = 1;
-	bit.value += bin << bit.i;
-	if (bit.status == false)
-		bit.pid_client += bin << bit.i;
+	(void) contex;
+	pid_client = info->si_pid;
+	if (signum == SIGUSR2)
+		bit.value += 1 << bit.i;
 	bit.i++;
-	if (bit.status == false && bit.i == 32)
-		client_pid(&bit);
-	if (bit.status == true && bit.i == 8)
-		write_msg(&bit);
+	if (bit.i == 8)
+	{
+		bit.i = 0;
+		write(1, &(bit.value), 1);
+		if (bit.value == 0)
+			kill(pid_client, SIGUSR2);
+		bit.value = 0;
+	}
+	kill(pid_client, SIGUSR1);
 }
 
 int	main(int argc, char **argv)
 {
 	struct sigaction	sign;
 
-	sign.sa_handler = get_bit;
 	sigemptyset(&sign.sa_mask);
 	sign.sa_flags = SA_SIGINFO;	
+	sign.sa_sigaction = get_bit;
 
 	if (argc != 1 && argv[0][0] != '\0')
 		return (ft_putstr_fd("Error\nServer does not take parameters\n", 2), -1);
